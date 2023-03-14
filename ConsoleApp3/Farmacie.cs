0a3348d2821ab427ebe306ebe716d00138bfd85b
LobiucAndra <127866136+LobiucAndra@users.noreply.github.com>
2023-03-14 16:21:52 +0200

    Initial commit

---
+++ ConsoleApp3/Farmacie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Farmacie
+    {
+        public void AdaugMed(string medicament,int cantitate) { }
+        public void AfisareListaMed(string nume_medicament) { }
+    }
+}
